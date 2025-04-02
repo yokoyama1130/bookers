30a0ac00e6983cd04d81f706da6351b23bda929b
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-02 20:24:56 +0000

    bookers_all_edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+
   def create
     book = Book.new(book_params)
     book.save
@@ -15,6 +21,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
   get '/top' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
