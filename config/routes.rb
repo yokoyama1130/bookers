8c72eddd51500097ba0e0fc2ad68415aef473444
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-02 19:44:21 +0000

    bookers

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/edit'
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
